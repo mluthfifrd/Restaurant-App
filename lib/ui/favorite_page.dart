dd983a7bb9a97ddff83086c69b2c97fc05f712cc
mluthfifrd <m.luthfifarid@gmail.com>
2024-03-03 17:56:54 +0700

    add list restaurant favorite

--- lib/ui/favorite_page.dart
+++ lib/ui/favorite_page.dart
@@ -1,4 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:restaurant_app/common/custom_widget/card_restaurant_list.dart';
+import 'package:restaurant_app/data/db/database_helper.dart';
+import 'package:restaurant_app/provider/database_provider.dart';
+
+import '../utils/result_state.dart';
 
 class FavoritePage extends StatelessWidget {
   static const String favoriteTitle = 'Favorit';
@@ -13,6 +19,33 @@ class FavoritePage extends StatelessWidget {
             style: Theme.of(context).textTheme.headlineSmall),
         automaticallyImplyLeading: false,
       ),
+      body: _buildList(),
     );
+  }
+
+  Widget _buildList() {
+    return ChangeNotifierProvider(
+        create: (_) => DatabaseProvider(databaseHelper: DatabaseHelper()),
+        child: Consumer<DatabaseProvider>(
+          builder: (context, provider, child) {
+            if (provider.state == ResultState.loading) {
+              return const Center(child: CircularProgressIndicator());
+            } else if (provider.state == ResultState.hasData) {
+              return ListView.builder(
+                itemCount: provider.bookmarks.length,
+                itemBuilder: (context, index) {
+                  return CardRestaurantList(
+                      restaurantList: provider.bookmarks[index]);
+                },
+              );
+            } else {
+              return Center(
+                child: Material(
+                  child: Text(provider.message),
+                ),
+              );
+            }
+          },
+        ));
   }
 }
